7f57871d30fd7ad6ab7f432ded0c4d88f9d62bf2
Zelltress <159699071+Zelltress@users.noreply.github.com>
2024-03-02 02:24:51 +0700

    Update Practic_3.py

--- Practic_3.py
+++ Practic_3.py
@@ -17,7 +17,7 @@ print(hour,':', min)
 # Exercise №3
 num = int(input())
 if num % 2 == 0: print('0')
-else: print('1')???????????????????????????
+else: print('1')
 
 # Exercise №4
 x, y, n = map(int, input().split())
@@ -49,25 +49,94 @@ h = 7*100
 w = 5*100
 way = h^2+w^2
 print(way)
-'''
 
 # Exercise №8
 import turtle
 
-def square(x, y, a):
+def square(x, y, a, b, r):
 
     turtle.up()
     turtle.setposition(x, y)
     turtle.down()
+    turtle.right(r)
     turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(b)
     turtle.right(90)
     turtle.forward(a)
     turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
+    turtle.forward(b)
     turtle.right(90)
 
+turtle.speed(10)
+square(0, 0, 50, 8, 0)
+square(50, 0, 170, 8, 70)
+square(66, -38, 300, 8, -70)
+square(90, -130, 250, 8, 0)
+square(110, -160, 200, 8, 0)
+
+turtle.up()
+turtle.setposition(160, -220)
+turtle.down()
+turtle.circle(20)
+
+turtle.up()
+turtle.setposition(280, -220)
+turtle.down()
+turtle.circle(20)
+
+square(100, -43, 125, 8, 70)
+square(180, -43, 125, 8, 10)
+square(280, -43, 125, 8, 20)
+
+
+
+# Exercise №10
+raw = input('Enter number:')
+num = str(raw)
+print(num)
+
+
+# Exercise №11
+import math
+
+a = int(input())
+b = int(input())
+c = int(input())
+A = int(math.degrees(math.acos((b**2 + c**2 - a**2)/(2*b*c))))
+B = int(math.degrees(math.acos((c**2 + a**2 - b**2)/(2*c*a))))
+C = int(math.degrees(math.acos((a**2 + b**2 - c**2)/(2*a*b))))
+
+print(A, B, C)
+
+
+# Exercise №12
+att = int(input())
+comp = int(input())
+yds = int(input())
+td = int(input())
+int = int(input())
+
+
+
+# Exercise №13
+x = int(input())
+y = int(input())
+print(('0', '1')[x // y * y == x])
+
+
+# Exercise №14
+n = float(input())
+
+
+
+
+# Exercise №15
+import datetime
+
+time = datetime.datetime.now()
+print(time)
+'''
 
 
 
