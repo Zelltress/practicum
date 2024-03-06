8ab7994629407233336c9406edc2166efaaa6948
Zelltress <159699071+Zelltress@users.noreply.github.com>
2024-03-06 11:38:11 +0700

    Create Practic_4.py

---
+++ Practic_4.py
@@ -0,0 +1,23 @@
+
+'''
+# Exercise №1
+code = input()
+if code == 'пароль':
+    print('Проходи!')
+else:
+    print('Доступ запрещен!')
+
+
+# Exercise №2
+wrds = input('Какие два слова передал первой радиограммой Александр Попов? ').split()
+frst_wrd = 'примите'
+scnd_wrd = 'радиограмму'
+print(wrds[0] == frst_wrd)
+print(wrds[1] == scnd_wrd)
+'''
+
+# Exercise №3
+qstn = input('')
+
+    
+
